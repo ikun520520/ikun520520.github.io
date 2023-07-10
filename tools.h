e71bb806aac3cd18046604e3d1947e50e3ebee07
ikun520520 <134181098+ikun520520@users.noreply.github.com>
2023-07-10 17:54:18 +0800

    Add files via upload
    Signed-off-by: ikun520520 <134181098+ikun520520@users.noreply.github.com>

---
+++ tools.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <graphics.h>
+
+void putimagePNG(int  picture_x, int picture_y, IMAGE* picture);
+int getDelay();
+//void _putimagePNG(int  picture_x, int picture_y, IMAGE* picture);
